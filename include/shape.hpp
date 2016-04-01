8606fab146ecdab19385f7359453a15a9f3c729c
Richard Lewis <richard.lewis@gold.ac.uk>
2016-04-01 17:25:01 +0100

    Added typedefs for bounds_t and point_t

--- include/shape.hpp
+++ include/shape.hpp
@@ -10,19 +10,22 @@ namespace musak {
 
     namespace annotations {
 
+        typedef std::tuple<int, int, int, int> bounds_t;
+        typedef std::pair<int, int> point_t;
+
         class shape {
         public:
             shape();
             shape(std::string& id, std::vector<mark>& marks);
            ~shape();
 
-            bool                                  empty();
-            const std::tuple<int, int, int, int>& bounds();
-            const std::pair<int, int>&            size();
-            const std::pair<int, int>&            offset();
-            void                                  translateTo(const std::pair<int, int>& offset);
-            double                                length();
-            double                                perimeter();
+            bool            empty();
+            const bounds_t& bounds();
+            const point_t&  size();
+            const point_t&  offset();
+            void            translateTo(const point_t& offset);
+            double          length();
+            double          perimeter();
 
         private:
             std::string id;
