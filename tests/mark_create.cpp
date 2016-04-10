0ac586953582ff0940fefb18ca286af7d64c7057
Richard Lewis <richard.lewis@gold.ac.uk>
2016-04-10 01:08:14 +0100

    Added test for creating mark instances

---
+++ tests/mark_create.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE mark_create
+#include <boost/test/unit_test.hpp>
+#include "mark.hpp"
+
+using namespace musak::annotations;
+
+BOOST_AUTO_TEST_CASE(mark_create)
+{
+    auto m = mark("#000000", 3, 0, 0, 10, 10, "08:54:12.490");
+    auto ts_value = hms_t(8, 54, 12, 490);
+    BOOST_CHECK(m.time().get_hms() == ts_value);
+}
