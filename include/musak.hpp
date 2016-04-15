787544d8479ad675732d1560325f8789b05331a2
Richard Lewis <richard.lewis@gold.ac.uk>
2016-04-16 00:35:55 +0100

    Added op<< implementation for bounds_t

--- include/musak.hpp
+++ include/musak.hpp
@@ -1,12 +1,17 @@
 #ifndef MUSAK_H
 #define MUSAK_H
 #include <tuple>
+#include <ostream>
 
 namespace musak {
 
     namespace annotations {
         using bounds_t = std::tuple<int, int, int, int>;
         using point_t = std::pair<int, int>;
+
+        inline std::ostream& operator<<(std::ostream& st, const bounds_t& b) {
+            st << "(" << std::get<0>(b) << "," << std::get<1>(b) << "," << std::get<2>(b) << "," << std::get<3>(b) << ")";
+        }
 
         constexpr bounds_t zero_bounds(std::make_tuple(0,0,0,0));
         constexpr point_t origin(std::make_pair(0,0));
