78871985ba6506fb9d3a48d9041ecde539db9663
Richard Lewis <richard.lewis@gold.ac.uk>
2016-04-16 00:35:06 +0100

    Removed unnecessary output from timestamp tests

--- tests/timestamp_read.cpp
+++ tests/timestamp_read.cpp
@@ -3,7 +3,6 @@
 #include "timestamp.hpp"
 #include <string>
 #include <sstream>
-#include <iostream>
 
 using namespace musak::annotations;
 
@@ -13,12 +12,7 @@ BOOST_AUTO_TEST_CASE(timestamp_show)
     timestamp ts1 = timestamp(ts1_str);
     std::ostringstream ts1_repr;
     ts1_repr << ts1;
-    std::cout << ts1_str << " == " << ts1_repr.str() << "?" << std::endl;
     BOOST_CHECK(ts1_str == ts1_repr.str());
-}
-
-std::ostream& operator<<(std::ostream& st, hms_t h) {
-    return st << "(" << std::get<0>(h) << "," << std::get<1>(h) << "," << std::get<2>(h) << "," << std::get<3>(h) << ")";
 }
 
 BOOST_AUTO_TEST_CASE(timestamp_parse)
@@ -26,6 +20,5 @@ BOOST_AUTO_TEST_CASE(timestamp_parse)
     std::string ts1_str = "19:20:52.426";
     timestamp ts1 = timestamp(ts1_str);
     hms_t ts1_value = hms_t(19, 20, 52, 426);
-    std::cout << ts1.get_hms() << " == " << ts1_value << "?" << std::endl;
     BOOST_CHECK(ts1.get_hms() == ts1_value);
 }
