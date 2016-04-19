15a57ae6fa25450999eb4e45ad350b4915690227
Richard Lewis <richard.lewis@gold.ac.uk>
2016-04-19 19:37:03 +0100

    Added op<< implementation for point_t

--- include/musak.hpp
+++ include/musak.hpp
@@ -13,6 +13,10 @@ namespace musak {
             st << "(" << std::get<0>(b) << "," << std::get<1>(b) << "," << std::get<2>(b) << "," << std::get<3>(b) << ")";
         }
 
+        inline std::ostream& operator<<(std::ostream& st, const point_t& p) {
+            st << "(" << p.first << "," << p.second << ")";
+        }
+
         constexpr bounds_t zero_bounds(std::make_tuple(0,0,0,0));
         constexpr point_t origin(std::make_pair(0,0));
 
